3c964b83ec437a8f40ba59057d76bea5a3d00655
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-07-28 21:07:38 -0600

    added tests for part3

--- movieList/movie.test.ts
+++ movieList/movie.test.ts
@@ -14,10 +14,19 @@ afterAll(async () => {
     await driver.quit()
 })
 
-test ('Adding a movie to page', async () => {
+test ('Adding a movie to page, crossing off, then deleting', async () => {
 
     let inputField = await driver.findElement(By.css('input')).sendKeys('Jurassic Park')
     let addBtn = await driver.findElement(By.css('button'))
     await addBtn.click()
-     await driver.sleep(6000)
+     await driver.sleep(3000)
+
+     let crossOffMovie = await driver.findElement(By.xpath('//li/span'))
+     await crossOffMovie.click()
+     await driver.sleep(3000)
+     
+     let deleteMovie = await driver.findElement(By.xpath('//li/button'))
+     await deleteMovie.click()
+     await driver.sleep(3000)
+
 })
